dd4dc949dcd424d1251123762ba71a73893f8533
Jinah <monsatfri@gmail.com>
2023-12-13 23:34:27 +0900

    feat: signin, signup 화면에 form 태그 및 email input focus out 이벤트 핸들링 추가

---
+++ js/sign-common.js
@@ -0,0 +1,19 @@
+let emailInput = document.querySelector(".sign-email");
+
+emailInput.addEventListener("focusout", function (e) {
+  let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  let emailValue = this.value;
+
+  if (emailValue.trim() === "") {
+    document.getElementById("email-error-message").textContent =
+      "이메일을 입력해주세요.";
+    this.classList.add("input-error");
+  } else if (!emailPattern.test(emailValue)) {
+    document.getElementById("email-error-message").textContent =
+      "올바른 이메일 형식이 아닙니다.";
+    this.classList.add("input-error");
+  } else {
+    document.getElementById("email-error-message").textContent = "";
+    emailInput.classList.remove("input-error");
+  }
+});
