aa9d31434187572e217c71c75f7addf529f5cc73
Jinah <monsatfri@gmail.com>
2023-12-24 03:25:43 +0900

    feat: 로그인 기능을 Axios를 활용하여 API에 전송하는 기능 추가

--- js/signin.js
+++ js/signin.js
@@ -10,18 +10,27 @@ function attemptSignin() {
   const email = emailInput.value;
   const password = passwordInput.value;
 
-  if (email === "test@codeit.com" && password === "codeit101") {
-    signinForm.submit();
-    return;
-  }
-
   if (!passwordPattern.test(passwordInput.value)) {
     document.getElementById("password-error-message").textContent =
       "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
     passwordInput.classList.add("input-error");
     return;
   }
+  signin(email, password);
+}
 
+function signin(email, password) {
+  axios
+    .post("https://bootcamp-api.codeit.kr/api/sign-in", { email, password })
+    .then((response) => {
+      let { data } = response.data;
+      localStorage.setItem("accessToken", data.accessToken);
+      location.href = "/folder";
+    })
+    .catch(signinError);
+}
+
+function signinError() {
   errorMessage.textContent = "이메일을 확인해주세요.";
   document.getElementById("password-error-message").textContent =
     "비밀번호를 확인해주세요.";
