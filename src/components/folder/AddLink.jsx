5f5b5163eabef2c64786f753066c9fd421e55353
Jinah <monsatfri@gmail.com>
2024-01-09 13:50:28 +0900

    feat: 스크롤 시 상단에 고정되지 않고 숨겨지도록 상단 네비게이션 바 스타일 조정

--- src/components/folder/AddLink.jsx
+++ src/components/folder/AddLink.jsx
@@ -1,17 +1,38 @@
 import link from 'img/ic-link.svg';
 import './addLink.scss';
+import { useEffect, useRef, useState } from 'react';
 
 export function AddLink({ text }) {
+  const ref = useRef(null);
+  const [scrollDirection, setScrollDirection] = useState('');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const parent = ref.current.parentElement;
+      if (parent.getBoundingClientRect().bottom < parent.clientHeight) {
+        setScrollDirection('floating');
+      } else {
+        setScrollDirection('');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <>
-      <div className="addLink">
+    <div ref={ref}>
+      <div className={`addLink ${scrollDirection}`}>
         <div className="addLink-frame">
           <img className="ic-link" alt="Link" src={link} />
           <input className="addLink-text" placeholder={text} />
           <button className="addLink-button">추가하기</button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
