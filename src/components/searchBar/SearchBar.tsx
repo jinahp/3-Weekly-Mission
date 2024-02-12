e9211794103c2a8beba0812f5fe5f022eba5db61
Jinah <monsatfri@gmail.com>
2024-02-12 16:33:22 +0900

    feat: 검색창에서 X 버튼 클릭 시 입력값 리셋 기능 추가

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,16 +1,35 @@
-import search from '@/img/ic-search.svg';
+import icSearch from '@/img/ic-search.svg';
+import icDelete from '@/img/ic-search-delete.svg';
 import './searchBar.scss';
+import { useState } from 'react';
 
 interface SearchBarProps {
   text: string;
 }
 
 const SearchBar = ({ text }: SearchBarProps) => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleDeleteClick = () => {
+    setSearchText('');
+  };
+
   return (
     <div className="search-bar">
       <div className="search-bar-frame">
-        <img className="ic-search" alt="Search" src={search} />
-        <input className="search-bar-text-wrapper" placeholder={text} />
+        <img className="ic-search" alt="Search" src={icSearch} />
+        <input
+          className="search-bar-text-wrapper"
+          placeholder={text}
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <img
+          className="ic-search-delete"
+          alt="search-delete"
+          src={icDelete}
+          onClick={handleDeleteClick}
+        />
       </div>
     </div>
   );
